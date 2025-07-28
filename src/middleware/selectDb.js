0ee5910abb2ed2fb236f99f4d7c7ce8265018ad6
talfumier <henri.talfumier@gmail.com>
2025-07-28 20:18:45 +0200

    Minor changes

--- src/middleware/selectDb.js
+++ src/middleware/selectDb.js
@@ -1,8 +1,12 @@
 export function selectDb(req, res, next) {
   try {
-    const origin = req.headers["x-app-origin"];
-    req.db =
-      origin === "internal" ? req.app.locals.db.prod : req.app.locals.db.test;
+    const origin = req.headers["X-App-Origin"];
+    // origin = dev (dev front-end app > local PC)
+    // origin = test (test front-end app > Vercel, Netlify ?)
+    // origin = prod (production front-end app > OVH server)
+    if (!origin || origin === "dev" || origin === "test")
+      req.db = req.app.locals.db.test;
+    else req.db = req.app.locals.db.prod;
     next();
   } catch (err) {
     next(err, req, res, next); //call error handler middleware
